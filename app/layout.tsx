7b778fa0a55adedbb69081f6ec2fb7c99327dbee
Martín Bravo <123977407+MartinEBravo@users.noreply.github.com>
2025-08-29 15:12:13 -0400

    fix(imports): global to globals

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Link from 'next/link';
 import Image from 'next/image';
-import '@/app/global.css';
+import '@/app/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
