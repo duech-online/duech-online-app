932a616bac1236eeee0507cbf5962ef265a15217
Vicente Thiele Muñoz <vthielemunoz@gmail.com>
2025-10-14 13:20:34 -0300

    Revert "FIX(db.ts): My configuration"
    This reverts commit d86538f12f277d87cfd604cce10672332061e6c0.

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -21,11 +21,11 @@ const pool = connectionString
       connectionTimeoutMillis: 5000,
     })
   : new Pool({
-      host: 'localhost',
-      port: 5433,
-      database: 'duech',
-      user: 'duech',
-      password: 'OrdenyProgreso123',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: parseInt(process.env.POSTGRES_PORT || '5432'),
+      database: process.env.POSTGRES_DB || 'duech',
+      user: process.env.POSTGRES_USER || 'duech',
+      password: process.env.POSTGRES_PASSWORD,
       max: 20,
       idleTimeoutMillis: 30000,
       connectionTimeoutMillis: 2000,
