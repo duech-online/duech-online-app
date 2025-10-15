3a20aca315961577c42522a8c58610edb27b4296
Martín Bravo <123977407+MartinEBravo@users.noreply.github.com>
2025-10-15 13:57:30 -0300

    refactor(LoginForm): move icon components to icons.tsx

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -5,58 +5,8 @@ import { Button } from '@/app/ui/button';
 import { useState } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
 import { authenticate } from '@/app/lib/actions';
+import { AtSymbolIcon, KeyIcon, ExclamationCircleIcon, ArrowRightIcon } from '@/app/ui/icons';
 
-function AtSymbolIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" {...props}>
-      <path
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M16 12a4 4 0 10-8 0 4 4 0 008 0v-1a1 1 0 112 0v1a6 6 0 11-6-6"
-      />
-    </svg>
-  );
-}
-
-function KeyIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" {...props}>
-      <path
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M15 7a4 4 0 11-4 4l-6 6v-3h-3v-3h3v-3h3l3-3a4 4 0 014-1z"
-      />
-    </svg>
-  );
-}
-
-function ExclamationCircleIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" {...props}>
-      <path
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M12 9v4m0 4h.01M12 2a10 10 0 100 20 10 10 0 000-20z"
-      />
-    </svg>
-  );
-}
-
-function ArrowRightIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" {...props}>
-      <path
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M5 12h14m-7-7l7 7-7 7"
-      />
-    </svg>
-  );
-}
 
 export default function LoginForm() {
   const searchParams = useSearchParams();
