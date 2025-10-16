987ef5ef0270d823577b1895cd5fae64a0ba29ab
Martín Bravo <123977407+MartinEBravo@users.noreply.github.com>
2025-10-16 09:10:11 -0300

    chore(deps): update dependencies and add new script for deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
+// eslint.config.mjs
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,6 +15,23 @@ const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+
+    rules: {
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
   },
 ];
 
